b0efd632230a50ab4a521f0a1091f695fadf6af7
Thales971 <thales.t.silva6@aluno.senai.br>
2025-09-01 16:04:18 -0300

    feat: Adiciona post varinhas

--- server.js
+++ server.js
@@ -83,7 +83,91 @@ app.post('/bruxos', (req, res) => {
     });
 });
 
+app.get ('/varinhas', (req, res) => {
+        const { material, nucleo, comprimento } = req.query;
+        let resultado = varinhas;
+      
+        if (material) {
+          resultado = resultado.filter(v => v.material.toLowerCase() === material.toLowerCase());
+        }
+        if (nucleo) {  
+          resultado = resultado.filter(v => v.nucleo.toLowerCase() === nucleo.toLowerCase());
+        }
+        if (comprimento) {
+          resultado = resultado.filter(v => v.comprimento === comprimento);
+        } 
+
+        res.status(200).json({
+          total: resultado.length,
+          data: resultado
+        });
+    });
+
+    app.post('/varinhas', (req, res) => {   
+        const { material, nucleo, comprimento } = req.body; 
+    
+        if (!material || !nucleo || !comprimento) {
+            return res.status(400).json({
+                success: false,
+                message: "Material, núcleo e comprimento são obrigatórios para uma varinha!"
+            });
+        };
+
+        const novaVarinha = {
+            id: varinhas.length + 1,
+            material: material || "AINDA NAO DEFINIDO",
+            nucleo: nucleo || "AINDA NAO DEFINIDO",
+            comprimento: comprimento || "AINDA NAO DEFINIDO"
+        };
+
+        varinhas.push(novaVarinha);
+
+
+
+        res.status(201).json({
+            success: true,
+            message: "Nova varinha adicionada ao estoque de varinhas!",
+            data: novaVarinha
+        });
+    },);
+
+    app.get ('pocoes', (req, res) => {
+        const { nome, efeito, duracao } = req.query;
+        let resultado = pocoes; 
+
+        if (nome) {
+          resultado = resultado.filter(p => p.nome.toLowerCase().includes(nome.toLowerCase()));
+        }
+        if (efeito) {  
+          resultado = resultado.filter(p => p.efeito.toLowerCase().includes(efeito.toLowerCase()));
+        }
+        if (duracao) {
+          resultado = resultado.filter(p => p.duracao.toLowerCase().includes(duracao.toLowerCase()));
+        }
+        res.status(200).json({
+          total: resultado.length,
+          data: resultado
+        });
+    });
+
+    app.get ('/animais', (req, res) => {
+        const { nome, tipo, cor, dono } = req.query;
+        let resultado = animais; 
+        
+        if (nome) {
+          resultado = resultado.filter(a => a.nome.toLowerCase().includes(nome.toLowerCase()));
+        }
+        if (tipo) {  
+          resultado = resultado.filter(a => a.tipo.toLowerCase().includes(tipo.toLowerCase()));
+        }
+        res.status(200).json({
+          total: resultado.length,
+          data: resultado
+        });
+    });
+
 // Iniciar o servidor
 app.listen(serverPort, () => {
     console.log("Servidor esta rodando...")
 });
+    
